7c7b6345e27840ca09f03e41f2ba7b2ffb31bc5a
victorvaz001 <victorvaz001@gmail.com>
2023-11-29 11:35:57 -0300

    Order, Enum, relacionamento muitos-para-um

--- src/main/java/com/victorvaz/dscommerce/entities/User.java
+++ src/main/java/com/victorvaz/dscommerce/entities/User.java
@@ -3,6 +3,8 @@ package com.victorvaz.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "tb_user")
@@ -16,6 +18,9 @@ public class User {
     private String phone;
     private LocalDate birthDate;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User (){
     }
@@ -77,5 +82,8 @@ public class User {
         this.password = password;
     }
 
+    public List<Order> getOrders() {
+        return orders;
+    }
 
 }
