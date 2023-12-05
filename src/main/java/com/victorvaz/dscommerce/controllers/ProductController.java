fadb8f5523fa8504f45ab12f8640f937dc75fbd7
victorvaz001 <victorvaz001@gmail.com>
2023-12-05 11:06:20 -0300

    GET product by id

--- src/main/java/com/victorvaz/dscommerce/controllers/ProductController.java
+++ src/main/java/com/victorvaz/dscommerce/controllers/ProductController.java
@@ -1,9 +1,12 @@
 package com.victorvaz.dscommerce.controllers;
 
+import com.victorvaz.dscommerce.dto.ProductDTO;
 import com.victorvaz.dscommerce.entities.Product;
 import com.victorvaz.dscommerce.repositories.ProductRepository;
+import com.victorvaz.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,14 +17,13 @@ import java.util.Optional;
 public class ProductController {
 
     @Autowired
-    private ProductRepository repository;
+    private ProductService service;
 
-    @GetMapping
-    public String test() {
+    @GetMapping(value = "/{id}")
+    public ProductDTO findById(@PathVariable Long id) {
 
-        Optional<Product> result = repository.findById(1L);
-        Product product = result.get();
-        return product.getName();
+        ProductDTO dto = service.findById(id);
+        return dto;
 
     }
 
---
+++ src/main/java/com/victorvaz/dscommerce/services/ProductService.java
@@ -0,0 +1,25 @@
+package com.victorvaz.dscommerce.services;
+
+import com.victorvaz.dscommerce.dto.ProductDTO;
+import com.victorvaz.dscommerce.entities.Product;
+import com.victorvaz.dscommerce.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    @Transactional(readOnly = true)
+    public ProductDTO findById(Long id){
+
+        Product product = repository.findById(id).get();
+        return new ProductDTO(product);
+
+    }
+}
