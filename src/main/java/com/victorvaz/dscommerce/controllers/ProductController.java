fcb34765148a0b4b91508fcb9013c637eeb6cc47
victorvaz001 <victorvaz001@gmail.com>
2023-12-05 10:40:49 -0300

    Test controller /product, return nome do produto da base dados

--- src/main/java/com/victorvaz/dscommerce/controllers/ProductController.java
+++ src/main/java/com/victorvaz/dscommerce/controllers/ProductController.java
@@ -1,16 +1,28 @@
 package com.victorvaz.dscommerce.controllers;
 
+import com.victorvaz.dscommerce.entities.Product;
+import com.victorvaz.dscommerce.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/products")
 public class ProductController {
 
+    @Autowired
+    private ProductRepository repository;
+
     @GetMapping
-    public String test (){
-        return "Olá mundo!";
+    public String test() {
+
+        Optional<Product> result = repository.findById(1L);
+        Product product = result.get();
+        return product.getName();
+
     }
 
 }
