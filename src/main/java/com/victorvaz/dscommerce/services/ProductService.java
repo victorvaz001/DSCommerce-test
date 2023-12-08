66a2f2d89375864f06b514f768deaac22764259e
victorvaz001 <victorvaz001@gmail.com>
2023-12-08 16:31:50 -0300

    DELETE product

--- src/main/java/com/victorvaz/dscommerce/services/ProductService.java
+++ src/main/java/com/victorvaz/dscommerce/services/ProductService.java
@@ -47,8 +47,12 @@ public class ProductService {
         return new ProductDTO(entity);
     }
 
-    private void copyDtoToEntity(ProductDTO dto, Product entity) {
+    @Transactional
+    public void delete(Long id){
+        repository.deleteById(id);
+    }
 
+    private void copyDtoToEntity(ProductDTO dto, Product entity) {
         entity.setName(dto.getName());
         entity.setDescription(dto.getDescription());
         entity.setPrice(dto.getPrice());
